ce9b92e8b2ab7ad81d7add70a1f64e19144a4595
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-07 07:36:48 -0800

    Code clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#2F3845`,
-        theme_color: `#2F3845`,
+        theme_color: `#64FFDA`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
