feb95cb914581aa7d3640d6fe8b27b58e5bb69bb
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-07 17:55:14 -0800

    Fixed page content layout

--- src/components/pageConent/homepage.js
+++ src/components/pageConent/homepage.js
@@ -4,22 +4,19 @@ import Button from "../button"
 import "../../styles/layout.css"
 
 const homePage = () => (
-	<div className="homepage">
-		<p id="welcome">Hi my name is,</p>
-		<p id="name">Rojah Lewis</p>
-		<p id="title">Developer, Cyber Security enthusiast</p>
-		<p id="description">
+	<section className="homepage">
+		<h1>Hi, my name is,</h1 >
+		<h3>Rojah Lewis.</h3 >
+		<h2>Developer, Tech & Cyber Security enthusiast</h2>
+		<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.
 		</p>
-		{/* <a href="www.google.com"> */}
 			<Button
-				home="/about"
 				size="1.5em 4em"
 				label="Get in touch"
-			/>
-		{/* </a> */}
-		
-	</div>
+			></Button>
+	 </section>
+	
 )
 
 export default homePage;
