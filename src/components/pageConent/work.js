bf3af84d8fb90bdc896760d706392ca690855689
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-12 10:34:30 -0800

    Added work grid

--- src/components/pageConent/work.js
+++ src/components/pageConent/work.js
@@ -1,57 +1,142 @@
 import React from "react"
 
 import Github from "../../images/github.svg"
-
+import Extsign from "../../images/sign.svg"
 const work = () => (
 	 <section className="work">
 		 <h1 className="page-heading"> Work</h1>
-		 <div className="work-home">
-			 <div className="project-cards">
-				<header>
-					<div className="card-heading">
-						<a href="www.github.com/tenyson05">
-							<img src= { Github } alt="Github"/>
-						</a>
-					</div>
-					<div className="card-info">
-						<h1>Project</h1>
-						<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+		 <div classname="sub-work">
+			 <div className="projects">
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<a className="card-icon" href="/www.github.com/tenyson05">
+								<img src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							<h1>Project</h1>
+							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
 
-					</div>
-				</header>
-				<footer>
-					<div className="foot-info">
-						<p>Nodejs</p>
-						<p>Express</p>
-						<p>DigitalOcean</p>
-					</div>
-				</footer>
-			 </div>
-			 <div className="project-cards">
-				<header>
-					<div className="card-heading">
-						<a href="www.github.com/tenyson05">
-							<img src= { Github } alt="Github"/>
-						</a>
-					</div>
-					<div className="card-info">
-						<h1>Project</h1>
-						<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+						</div>
+					</header>
+					<footer>
+						<div className="foot-info">
+							<p>Nodejs</p>
+							<p>Express</p>
+							<p>DigitalOcean</p>
+						</div>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<a href="www.github.com/tenyson05">
+								<img src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							<h1>Project</h1>
+							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
 
-					</div>
-				</header>
-				<footer>
-					<div className="foot-info">
-						<p>Nodejs</p>
-						<p>Express</p>
-						<p>DigitalOcean</p>
-					</div>
-				</footer>
+						</div>
+					</header>
+					<footer>
+						<div className="foot-info">
+							<p>Nodejs</p>
+							<p>Express</p>
+							<p>DigitalOcean</p>
+						</div>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<a href="www.github.com/tenyson05">
+								<img src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							<h1>Project</h1>
+							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<div className="foot-info">
+							<p>Nodejs</p>
+							<p>Express</p>
+							<p>DigitalOcean</p>
+						</div>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<a href="www.github.com/tenyson05">
+								<img src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							<h1>Project</h1>
+							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<div className="foot-info">
+							<p>Nodejs</p>
+							<p>Express</p>
+							<p>DigitalOcean</p>
+						</div>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<a href="www.github.com/tenyson05">
+								<img src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							<h1>Project</h1>
+							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<div className="foot-info">
+							<p>Nodejs</p>
+							<p>Express</p>
+							<p>DigitalOcean</p>
+						</div>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<a href="www.github.com/tenyson05">
+								<img src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							<h1>Project</h1>
+							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<div className="foot-info">
+							<p>Nodejs</p>
+							<p>Express</p>
+							<p>DigitalOcean</p>
+						</div>
+					</footer>
+				</div>
+				
 			 </div>
-			 
-			
+
 		 </div>
-
 	 </section>
 )
 
