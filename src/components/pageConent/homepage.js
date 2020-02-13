801ca4ab07fa06fb3ad00147bed9b13d8081e71c
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-12 21:28:43 -0800

    Centered page layout

--- src/components/pageConent/homepage.js
+++ src/components/pageConent/homepage.js
@@ -1,20 +1,27 @@
 import React from "react"
 
+import "../../styles/homepage.css"
 import Button from "../button"
-import "../../styles/layout.css"
 
 const homePage = () => (
-	<section className="homepage">
-		<h1>Hi, my name is,</h1 >
-		<h3>Rojah Lewis.</h3 >
-		<h2>Developer, Tech & Cyber Security enthusiast</h2>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.
-		</p>
-		<Button
-			size="1.5em 4em"
-			label="Get in touch"
-		></Button>
+	<section id="home" className="homepage">
+		<h1 className="homepage-greet">Hi, my name is,</h1 >
+		<h2 className="homepage-name">Rojah Lewis.</h2 >
+		<h3 className="homepage-desc">Developer, Tech & Cyber Security enthusiast</h3>
+		
+		<div  className="desc">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.
+			</p>
+		</div>
+		<div>
+		<Button 
+			size="0.9em 2.5em"
+			go="/home"
+			label="Resume"
+		/>
+		</div>
+		
 	</section>
 	
 )
