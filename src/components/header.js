48e758c6500d18863688418679ed9de86bc5a5c0
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-12 21:26:02 -0800

    Added link to buttons

--- src/components/header.js
+++ src/components/header.js
@@ -9,24 +9,39 @@ import Button from "../components/button"
 
 
 const Header = () => (
-  <header>
-    <div className="header-layout">
+  <header className="main-header">
+	  <nav className="navbar">
+
 		<div className="nav-left-section">
 			<img style = {{ height: "50px", width: "50px", padding: "12px" }} src={ Game }/>
 		</div>
 		
 		<div className="nav-right-section">
-			<p id="header-text">01. <a href="/#about">About</a></p>
-			<p id="header-text">02. <a href="/">Skills</a></p>
-			<p id="header-text">03. <a href="/">Work</a></p>
-			<p id="header-text">04. <a href="/">Contact</a></p>
-			<Button 
-				size="0.6em 2em"
-				go="/home"
-				label="Resume"
-			/>
+			<ol className="header-list">
+				<li className="header-text">
+					<a href="/#about">About</a>
+				</li>
+				<li className="header-text">
+					<a href="/#skills">Skills</a>
+				</li>
+				<li className="header-text">
+					<a href="/#work">Work</a>
+				</li>
+				<li className="header-text">
+					<a href="/#contact">Contact</a>
+				</li>
+			</ol>
+			<div>
+				<Button 
+					size="0.6em 2em"
+					go="/home"
+					label="Resume"
+				/>
+			</div>
+			
 		</div>
-    </div>
+	  </nav>
+
   </header>
 )
 
