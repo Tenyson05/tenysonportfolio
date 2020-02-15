2870e7dc2b51ff04fc971088dcc9c1ad03ee037f
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-14 21:36:19 -0800

    Environment package updates

---
+++ src/components/pageConent/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const contact = () => (
+	<section className="contact">
+		<h1 className="page-heading">Contact</h1>
+		<div className="contact-center">
+			<h1>GET IN TOUCH</h1>>
+		</div>
+
+	</section>
+)
+
+export default contact;
