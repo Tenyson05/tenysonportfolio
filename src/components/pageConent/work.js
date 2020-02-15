b79da9e09b99fe5b4fca4c1120681ee0252c2b9c
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-14 21:31:15 -0800

    Created card hover effect

--- src/components/pageConent/work.js
+++ src/components/pageConent/work.js
@@ -12,25 +12,136 @@ const work = () => (
 				<div className="project-cards">
 					<header>
 						<div className="card-heading">
-							<a className="card-icon" href="/www.github.com/tenyson05">
-								<img src= { Github } alt="Github"/>
+							<h1>Project</h1>
+							<a className="card-img" href="/www.github.com/tenyson05">
+								<img  src= { Github } alt="Github"/>
 							</a>
 						</div>
 						<div className="card-info">
-							<h1>Project</h1>
-							<p>consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+							
+							<p className="card-content">consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
 
 						</div>
 					</header>
 					<footer>
-						<div className="foot-info">
-							<p>Nodejs</p>
-							<p>Express</p>
-							<p>DigitalOcean</p>
-						</div>
+						<ul className="foot-info">
+							<li>Nodejs</li>
+							<li>Express</li>
+							<li>MySQL</li>
+						</ul>
 					</footer>
 				</div>
 				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<h1>Project</h1>
+							<a className="card-img" href="/www.github.com/tenyson05">
+								<img  src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							
+							<p className="card-content">consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<ul className="foot-info">
+							<li>Nodejs</li>
+							<li>Express</li>
+							<li>MySQL</li>
+						</ul>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<h1>Project</h1>
+							<a className="card-img" href="/www.github.com/tenyson05">
+								<img  src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							
+							<p className="card-content">consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<ul className="foot-info">
+							<li>Nodejs</li>
+							<li>Express</li>
+							<li>MySQL</li>
+						</ul>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<h1>Project</h1>
+							<a className="card-img" href="/www.github.com/tenyson05">
+								<img  src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							
+							<p className="card-content">consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<ul className="foot-info">
+							<li>Nodejs</li>
+							<li>Express</li>
+							<li>MySQL</li>
+						</ul>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<h1>Project</h1>
+							<a className="card-img" href="/www.github.com/tenyson05">
+								<img  src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							
+							<p className="card-content">consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<ul className="foot-info">
+							<li>Nodejs</li>
+							<li>Express</li>
+							<li>MySQL</li>
+						</ul>
+					</footer>
+				</div>
+				<div className="project-cards">
+					<header>
+						<div className="card-heading">
+							<h1>Project</h1>
+							<a className="card-img" href="/www.github.com/tenyson05">
+								<img  src= { Github } alt="Github"/>
+							</a>
+						</div>
+						<div className="card-info">
+							
+							<p className="card-content">consectetur adipiscing elit. Cras ultricies ultricies turpis, a posuere nisi ullamcorper id. Curabitur egestas vehicula nibh non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies ultricies turpis,</p>
+
+						</div>
+					</header>
+					<footer>
+						<ul className="foot-info">
+							<li>Nodejs</li>
+							<li>Express</li>
+							<li>MySQL</li>
+						</ul>
+					</footer>
+				</div>
+				{/* <div className="project-cards">
 					<header>
 						<div className="card-heading">
 							<a href="www.github.com/tenyson05">
@@ -135,7 +246,7 @@ const work = () => (
 						</div>
 					</footer>
 				</div>
-				
+				 */}
 			 </div>
 
 		 </div>
