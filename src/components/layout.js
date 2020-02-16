bc2da1052ce39d8d2f4c66fdd17aa5d623117f4e
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-15 20:40:43 -0800

    Changed social images to svg

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,9 +11,9 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "../styles/main.css"
-import Github from "../images/github.svg"
-import Twitter from "../images/twitter.png"
-import Linkedin from "../images/linkedin.png"
+import Github from "./icon/github"
+import LinkedIn from "./icon/linkedin"
+import Twitter from "./icon/twitter"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -35,17 +35,17 @@ const Layout = ({ children }) => {
 					<ul className="social-links">
 						<li>
 							<a className="list-icon" href="https://github.com/Tenyson05" target="_blank" rel="nofollow nooperner noreferrer" >
-								<img src={ Github } alt="Github" title="Github"/>
+								<Github />
 							</a>
 						</li>
 						<li>
 							<a className="list-icon" href="https://twitter.com/Tenyson05_" target="_blank" rel="nofollow nooperner noreferrer" >
-								<img src={ Twitter } alt="Twitter" title="Twitter"/> 
+								<Twitter />
 							</a>
 						</li>
 						<li>
 							<a  href="https://www.linkedin.com/in/rojah-lewis-9652ab15b/" target="_blank" className="list-icon" rel="nofollow nooperner noreferrer" >
-								<img src={ Linkedin } alt="Linkedin" title="Linkedin"/>
+								<LinkedIn />
 							</a>
 						</li>
 					</ul>
