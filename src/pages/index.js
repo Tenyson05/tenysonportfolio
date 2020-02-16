1d9e91abf7ca3ea0a40f3287f01a47d9823a2ed3
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-16 10:59:15 -0800

    Code clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Homepage from "../components/pageConent/homepage"
 import About from "../components/pageConent/about"
 import Skills from "../components/pageConent/skills"
 import Work from "../components/pageConent/work"
+import SideBar from "../components/hamburger"
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Tenyson" />
 	<Homepage />
+	{/* <SideBar /> */}
 	<About />
 	<Skills />
 	<Work />
