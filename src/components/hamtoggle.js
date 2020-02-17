da97162f565c434f00c121d7f6bdb2bf9bd1fdeb
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-16 19:39:58 -0800

    Created hamburger menu

---
+++ src/components/hamtoggle.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+import "../styles/hamburger.css"
+import $ from "jQuery"
+
+// remember to try somethyign out with JQuery eventlistener on click
+
+const hamToggle = props => {
+	let hamburg;
+	if(props.sidebarclicker) {
+		$('.hamburger-sub').toggleClass('hamburger-sub-open');
+	}else {
+		$('.hamburger-sub-open').toggleClass('hamburger-sub');
+	}
+	// $('.hamburger').click(function() {
+	// 	// $(this).removeClass('hamburger-sub');
+	// 	$('.hamburger-sub').toggleClass('hamburger-sub-open');
+	// 	$('.hamburger-sub-open').toggleClass('hamburger-sub');
+	// });
+	return (
+		<div className="hamburger" onClick={props.sidebarclicker}>
+			<div className="hamburger-list">
+				<div className="hamburger-sub" >
+
+				</div>
+			</div>
+
+		</div>
+	)
+
+}
+
+export default hamToggle;
+
+{/* <div className="hamburger">
+			<div className="hamburger-list">
+				<div className="hamburger-sub">
+					
+				</div>
+			</div>
+		</div> */}
