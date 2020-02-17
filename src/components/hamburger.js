f1922d823440d3d39ae4102323581564912da35d
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-02-16 19:39:02 -0800

    Created the hamburger menu

---
+++ src/components/hamburger.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+import "../styles/hamburger.css"
+
+import Button from "./button"
+
+const sideBar = props => {
+	let sidebarMenu = "sidebar-menu"
+	if(props.show) {
+		sidebarMenu = "sidebar-menu open";
+	}
+	return (
+		<div className={sidebarMenu}>
+			<aside className="side-menu">
+				<nav className="side-nav">
+					<ol className="side-list">
+						<li className="side-list-item">
+							<a href="/#about">About</a>
+						</li>
+						<li className="side-list-item">
+							<a href="/#about">Skills</a>
+						</li>
+						<li className="side-list-item">
+							<a href="/#about">Work</a>
+						</li>
+					</ol>
+					<a href=".">
+						<Button
+							size="2em 3em"
+							label="Resume"
+						/>
+					</a>
+				</nav>
+			</aside>
+		</div>
+	)
+
+	}
+
+export default sideBar;
