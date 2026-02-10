95483d156e4853178a667c7fb9301145bbd86a9e
Tenyson05 <rojahog@hotmail.com>
2026-02-10 12:05:37 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,27 +35,16 @@ const Layout = ({ children }) => {
 				<div className="side-links">
 					<ul className="social-links">
 						<li>
-							<a className="list-icon" href="https://github.com/Tenyson05" target="_blank" rel="nofollow nooperner noreferrer" >
-								<Github />
-							</a>
 						</li>
 						<li>
-							<a className="list-icon" href="https://twitter.com/Tenyson05_" target="_blank" rel="nofollow nooperner noreferrer" >
-								<Twitter />
-							</a>
 						</li>
 						<li>
-							<a  href="https://www.linkedin.com/in/rojah-lewis-9652ab15b/" target="_blank" className="list-icon" rel="nofollow nooperner noreferrer" >
-								<LinkedIn />
-							</a>
+
 						</li>
 					</ul>
 				</div>
 				<div className="side-section">
 					<div className="side-email">
-						<a href="mailto:Rojahoh@hotmail.com" className="email-link">
-							Rojahog@hotmail.com
-						</a>
 					</div>
 				</div>
 				<div id="content">
