28f3205d202de875059d798fc6c46915c5e17a41
Tenyson05 <rojahog@hotmail.com>
2026-02-10 13:40:50 -0500

    Update about.js

--- src/components/pageConent/about.js
+++ src/components/pageConent/about.js
@@ -9,11 +9,6 @@ const about = () => (
 		<h3 className="page-heading">About</h3>
 		<div className="about-info">
 			<p>
-			Final year university student studying computer science at the University Of Technology, Jamaica and an aspiring 
-			cybersecurity analyst/engineer. I enjoy being a developer and a cybersecurity enthusiast which allows me to build
-			applications, software or websites with the added benefit of keeping them secure and safe from vulnerabilities 
-			and threats. During my spare time, I enjoy coding, watching anime and playing videos. My final goal is to become 
-			a game developer and develop a virtual world that grants users total control over their environment.
 			</p>
 			</div>
 		</div>
